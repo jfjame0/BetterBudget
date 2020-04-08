2b15bf73b7cfa74eba411bf8f92c417ece6ef424
John James <jjames_02@me.com>
2020-04-08 10:22:26 -0700

    The basic setup of the BudgetViewController has been established, in line with the tab bar controller. No data feeds to the cells yet. There is a tableview on the left for expenses, and a collection view that scrolls horizontally on the right. Contstraints will need to be looked at in landscape mode. From here, a split view controller may need to be looked at an experimented with.

---
+++ BetterBudget/Controllers/BudgetViewController.swift
@@ -0,0 +1,78 @@
+//
+//  BudgetViewController.swift
+//  BetterBudget
+//
+//  Created by JOHN JAMES III on 4/7/20.
+//  Copyright © 2020 JOHN JAMES III. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import CloudKit
+
+class BudgetCell: UICollectionViewCell {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+}
+
+class ExpensesInBudgetTV: UITableView, NSFetchedResultsControllerDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+}
+
+class BudgetViewController: UIViewController, NSFetchedResultsControllerDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private lazy var dataProvider: ExpenseProvider = {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let provider = ExpenseProvider(with: appDelegate!.coreDataStack.persistentContainer,
+                                       fetchedResultsControllerDelegate: self)
+        return provider
+    }()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if let flowLayout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.itemSize = CGSize(width: self.collectionView.bounds.width / 2, height: self.collectionView.bounds.height)
+            
+        }
+ 
+    }
+
+}
+
+extension BudgetViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 54
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "budgetCell", for: indexPath) as! BudgetCell
+        cell.textLabel.text = "Pay Date " + String(indexPath.row + 1)
+        return cell
+    }
+}
+
+extension BudgetViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item + 1)
+    }
+}
+
+
